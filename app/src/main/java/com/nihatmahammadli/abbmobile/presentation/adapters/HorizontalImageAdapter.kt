63ad6acbfab3b4e781c4a274b1384379adf0bf0c
Nihat <154012759+nihatmahammadli@users.noreply.github.com>
2025-07-10 19:28:55 +0400

    home page 40% added

---
+++ app/src/main/java/com/nihatmahammadli/abbmobile/presentation/adapters/HorizontalImageAdapter.kt
@@ -0,0 +1,30 @@
+package com.nihatmahammadli.abbmobile.presentation.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nihatmahammadli.abbmobile.databinding.ItemRecyclerHomepageBinding
+
+class HorizontalImageAdapter(val imageList: List<Int>) : RecyclerView.Adapter<HorizontalImageAdapter.ImageViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HorizontalImageAdapter.ImageViewHolder {
+        val binding = ItemRecyclerHomepageBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ImageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: HorizontalImageAdapter.ImageViewHolder,
+        position: Int
+    ) {
+        val image = imageList[position]
+        holder.binding.images.setImageResource(image)
+    }
+
+    override fun getItemCount(): Int {return imageList.size
+    }
+    inner class ImageViewHolder(val binding: ItemRecyclerHomepageBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
+}
--- app/src/main/java/com/nihatmahammadli/abbmobile/presentation/dashboard/home/HomePage.kt
+++ app/src/main/java/com/nihatmahammadli/abbmobile/presentation/dashboard/home/HomePage.kt
@@ -10,11 +10,14 @@ import android.widget.Toast
 import com.nihatmahammadli.abbmobile.databinding.FragmentHomePageBinding
 import androidx.core.content.edit
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.nihatmahammadli.abbmobile.R
+import com.nihatmahammadli.abbmobile.presentation.adapters.HorizontalImageAdapter
 
 class HomePage : Fragment() {
     private lateinit var binding: FragmentHomePageBinding
+    private lateinit var adapter: HorizontalImageAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,6 +27,7 @@ class HomePage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomePageBinding.inflate(inflater,container,false)
+        setupAdapter()
         return binding.root
     }
 
@@ -33,7 +37,20 @@ class HomePage : Fragment() {
             val sharedPref = requireActivity().getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
             sharedPref.edit { putBoolean("isLoggedIn", false) }
             findNavController().navigate(R.id.action_homePage_to_mainFragment)
+
         }
+    }
+
+    fun setupAdapter(){
+        val imageList = listOf(
+            R.drawable.read_more_1,
+            R.drawable.read_more_2,
+            R.drawable.read_more_3,
+        )
+        adapter = HorizontalImageAdapter(imageList)
+        binding.horizontalRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.horizontalRecyclerView.adapter = adapter
     }
 
 
