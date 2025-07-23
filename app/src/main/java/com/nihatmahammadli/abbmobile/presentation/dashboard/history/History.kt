65135a3b1eea674bdcd69979c8aaf41b0f436ba3
Nihat <154012759+nihatmahammadli@users.noreply.github.com>
2025-07-24 00:35:21 +0400

    history ui added (demo)

--- app/src/main/java/com/nihatmahammadli/abbmobile/presentation/dashboard/history/History.kt
+++ app/src/main/java/com/nihatmahammadli/abbmobile/presentation/dashboard/history/History.kt
@@ -6,9 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nihatmahammadli.abbmobile.R
+import com.nihatmahammadli.abbmobile.databinding.FragmentHistoryBinding
 
 class History : Fragment() {
-
+    private lateinit var binding: FragmentHistoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,7 +17,8 @@ class History : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = FragmentHistoryBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 }
